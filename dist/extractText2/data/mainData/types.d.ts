f53388405c8963dd9be4b9a64cd68e20669b528e
Sigureya <sigureya1208@gmail.com>
2025-02-04 18:43:56 +0900

    テキスト抽出を効率化した

---
+++ dist/extractText2/data/mainData/types.d.ts
@@ -0,0 +1,10 @@
+import { PickByType } from '@sigureya/rpgtypes';
+export interface ExtractedText {
+    key: string;
+    text: string;
+    id: number;
+}
+export type TextExtractable<T> = PickByType<T, string> & {
+    id: number;
+    note: string;
+};
