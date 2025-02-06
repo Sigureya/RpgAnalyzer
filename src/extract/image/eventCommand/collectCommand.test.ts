80f4b5423b2049c9ffa0fdeb1a06fedd818345ce
Sigureya <sigureya1208@gmail.com>
2025-02-06 09:59:08 +0900

    モックを外部から持ってくるように修正

--- src/extract/image/eventCommand/collectCommand.test.ts
+++ src/extract/image/eventCommand/collectCommand.test.ts
@@ -1,30 +1,43 @@
 import { describe, expect, test } from "vitest";
 import type * as RPG from "@sigureya/rpgtypes";
 import { isImageCommand } from "@sigureya/rpg-data-tools";
+import * as RmmmzMock from "@sigureya/rmmzmock";
 
 describe("isImageCommand", () => {
   test("showPicture", () => {
-    const command: RPG.Command_ShowPicture = {
-      code: 231,
-      indent: 0,
-      parameters: [1, "test", 0, 0, 100, 100, 100, 100, 0],
-    };
+    const command: RPG.Command_ShowPicture = RmmmzMock.MockShowPicture;
     expect(isImageCommand(command)).toBeTruthy();
   });
   test("changeActorImages", () => {
-    const command: RPG.Command_ChangeActorImages = {
-      code: 322,
-      indent: 0,
-      parameters: [0, "character", 0, "face", 0, "sv_battler"],
-    };
+    const command: RPG.Command_ChangeActorImages =
+      RmmmzMock.MockChangeActorImages;
     expect(isImageCommand(command)).toBeTruthy();
   });
   test("changeBattleBackground", () => {
-    const command: RPG.Command_ChangeBattleBackground = {
-      code: 283,
-      indent: 0,
-      parameters: ["bg1", "bg2"],
-    };
+    const command: RPG.Command_ChangeBattleBackground =
+      RmmmzMock.MockChangeBattleBackground;
     expect(isImageCommand(command)).toBeTruthy();
+  });
+  test("changeParallax", () => {
+    const command: RPG.Command_ChangeParallax = RmmmzMock.MockChangeParallax;
+    expect(isImageCommand(command)).toBeTruthy();
+  });
+  test("changeVehicleImage", () => {
+    const command: RPG.Command_ChangeVehicleImage =
+      RmmmzMock.MockChangeVehicleImage;
+    expect(isImageCommand(command)).toBeTruthy();
+  });
+});
+describe("isImageCommand falsy", () => {
+  const list = [
+    RmmmzMock.MockPlayBGM,
+    RmmmzMock.MockPlayBGS,
+    RmmmzMock.MockPlayME,
+    RmmmzMock.MockPlaySE,
+    RmmmzMock.MockChangeBattleBGM,
+  ];
+  test("audio", () => {
+    const result = list.filter(isImageCommand);
+    expect(result).toEqual([]);
   });
 });
