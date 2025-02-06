6eec0f8bd89308442bcbf0410b13b7e642e00deb
Sigureya <sigureya1208@gmail.com>
2025-02-06 17:36:28 +0900

    変数操作あれこれ

---
+++ src/extract/variable/correct.ts
@@ -0,0 +1,7 @@
+import type { EventCommand } from "@sigureya/rpgtypes";
+
+const collectVariableReadCommand = (
+  page: { list: EventCommand[] },
+  pageIndex: number,
+  event: { id: number }
+) => {};
