d3a59ffbcae6019e78da551120d258fecb62edf5
Sigureya <sigureya1208@gmail.com>
2025-02-11 14:04:45 +0900

    変数に書き込むを行うイベントコマンドの定義

---
+++ src/extract/variable/eventCommand/write/types.ts
@@ -0,0 +1,10 @@
+import type { Command_ControlVariables } from "@sigureya/rpgtypes";
+import type { CommandTypes } from "./detail/types";
+
+export interface ExtractedVariableWrite {
+  code: number;
+  variableId: number;
+}
+export type Command_VariableWrite =
+  | CommandTypes[keyof CommandTypes]
+  | Command_ControlVariables;
