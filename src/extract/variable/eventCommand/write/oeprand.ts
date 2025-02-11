3657fbd8a57c4ffc29d38c677b81c34495f1f8a2
Sigureya <sigureya1208@gmail.com>
2025-02-11 14:05:09 +0900

    「変数の操作」で使われるオペランド情報

---
+++ src/extract/variable/eventCommand/write/oeprand.ts
@@ -0,0 +1,12 @@
+import type {
+  Operand_Constatant,
+  Operand_Variable,
+  Operand_ItemData,
+  Operand_LastData,
+  Operand_Script,
+  Operand_Random,
+} from "@sigureya/rpgtypes";
+
+const operandConstant = (ope: Operand_Constatant) => {};
+
+// const operandVariable;
