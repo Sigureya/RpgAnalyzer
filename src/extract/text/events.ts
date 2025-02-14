510ceabb1533b7a1b7cf5eb63240b6fd53de2819
Sigureya <sigureya1208@gmail.com>
2025-02-14 22:39:25 +0900

    correct()を追加、掘り進むときに使う関数

--- src/extract/text/events.ts
+++ src/extract/text/events.ts
@@ -2,22 +2,35 @@ import type { CommandParameter } from "@sigureya/rpg-data-tools";
 import type {
   Data_CommonEvent,
   Data_Troop,
+  EventCommand,
   MapEvent,
 } from "@sigureya/rpgtypes";
-import type {
-  ExtractedCommonEventText,
-  ExtractedMapEventText,
-  ExtractedTroopText,
-} from "./types";
+import type { ExtractedCommonEventText, ExtractedTroopText } from "./types";
 import { extractNoteText } from "./mainData";
 import {
   extractTextFromEventCommands,
   extractTextFromEventPages,
 } from "./eventCommand";
+import type { ExtractedEventText } from "./eventCommand/types";
+
+export const correctTextCommands = (
+  page: { list: EventCommand[] },
+  pageIndex: number,
+  event: { id: number }
+): ExtractedEventText => {
+  const list: CommandParameter<string>[][] = extractTextFromEventCommands(
+    page.list
+  );
+  return {
+    eventId: event.id,
+    pageIndex,
+    commands: list.flat(),
+  };
+};
 
 export const correctMapEventText = (
   event: Pick<MapEvent, "note" | "pages" | "id">
-): ExtractedMapEventText => {
+) => {
   const commands: CommandParameter<string>[][][] =
     extractTextFromEventPages(event);
   return {
