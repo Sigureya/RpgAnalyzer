de9da9bc4d8d6b83ff0695f74c7e2d626cee4b47
Sigureya <sigureya1208@gmail.com>
2025-02-16 13:30:47 +0900

    テストケースを追加。expectedの型も明確にし、安全性を高めている

--- src/mapEvent.test.ts
+++ src/mapEvent.test.ts
@@ -2,7 +2,8 @@ import { describe, expect, test } from "vitest";
 import * as RpgTypes from "@sigureya/rpgtypes";
 import { createEventCommand } from "@sigureya/rpg-data-tools";
 import { extractEventCommandData } from "./mapEvent";
-import type { AudioCommandInfo } from "./extract";
+
+import type * as $ from "./extract/resultTypes";
 
 const createCommandList = () => {
   const callCommonnEvent: RpgTypes.Command_CommonEvent = createEventCommand(
@@ -20,14 +21,19 @@ const createCommandList = () => {
       },
     ]
   );
-  return [callCommonnEvent, playBgm];
+
+  const showPicture: RpgTypes.Command_ShowPicture = createEventCommand(
+    RpgTypes.SHOW_PICTURE,
+    [1, "actor1_1", 0, 0, 100, 100, 100, 100, 255, 0]
+  );
+  return [callCommonnEvent, playBgm, showPicture];
 };
 
 describe("extractEventCommandData", () => {
   const commandList = createCommandList();
   const result = extractEventCommandData(commandList);
   test("should extract audio", () => {
-    const expected: AudioCommandInfo = {
+    const expected: $.AudioCommandInfo = {
       code: RpgTypes.PLAY_BGM,
       path: {
         folder: "bgm",
@@ -38,5 +44,16 @@ describe("extractEventCommandData", () => {
   });
   test("should extract calls", () => {
     expect(result.calls).toEqual({ 123: 1 });
+  });
+  test("should extract images", () => {
+    const expected: $.ImageCommand = {
+      command: {
+        code: RpgTypes.SHOW_PICTURE,
+        paramIndex: 1,
+        value: "actor1_1",
+      },
+      folderName: "pictures",
+    };
+    expect(result.images).toEqual([expected]);
   });
 });
