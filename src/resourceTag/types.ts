2d56acf6102c27d53cc67c4bc72d48c18852a814
Sigureya <sigureya1208@gmail.com>
2025-02-18 23:38:53 +0900

    ResourcePath型を追加。ただし将来的には別リポジトリに担当させるかも

---
+++ src/resourceTag/types.ts
@@ -0,0 +1,20 @@
+import type { ImageFolders } from "@sigureya/rpgtypes";
+
+export interface ResourcePath {
+  folderName: string;
+  fileName: string;
+}
+
+export interface ResourceTagEx<T> {
+  tagName: string;
+  baseFolder: string;
+  list: T[];
+  assets: unknown;
+}
+
+export interface ImageResourcePath {
+  folder: ImageFolders;
+  fileName: string;
+}
+
+const xxxx = () => {};
