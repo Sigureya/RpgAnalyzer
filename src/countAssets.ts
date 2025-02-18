a616f5117c7de56ef1a344c502b4ac3387d71bc1
Sigureya <sigureya1208@gmail.com>
2025-02-19 01:01:37 +0900

    対応データを追加

--- src/countAssets.ts
+++ src/countAssets.ts
@@ -1,5 +1,11 @@
 import { countMatches } from "./countMatches";
-import type { ImageCommand, AudioCommandInfo } from "./extract";
+import type {
+  ImageCommand,
+  AudioCommandInfo,
+  VariableRead,
+  CommonEventCallCount,
+  VariableWrite,
+} from "./extract";
 
 export const countMatchesImage = (
   list: ReadonlyArray<ImageCommand>,
@@ -21,12 +27,26 @@ export const countMatchesAudio = (
     (item) => `${item.path.folder}:${item.path.fileName}`
   );
 
-// export const countMatchesVariable = (
-//   list: ReadonlyArray<VariableRead>,
-//   set: ReadonlySet<number>
-// ): number => countMatches(list, set, (item) => item.variableId);
+export const countMatchesVariable = (
+  list: ReadonlyArray<VariableRead>,
+  set: ReadonlySet<number>
+): number => countMatches(list, set, (item) => item.variableId);
 
-// export const countMatchesVariableWrite = (
-//   list: ReadonlyArray<VariableWrite>,
-//   set: ReadonlySet<number>
-// ): number => countMatches(list, set, (item) => item.variableId);
+export const countMatchesVariableWrite = (
+  list: ReadonlyArray<VariableWrite>,
+  set: ReadonlySet<number>
+): number => countMatches(list, set, (item) => item.variableId);
+
+/**
+ * @description Counts the total number of times specific common events have been called.
+ *
+ * @param eventCallCount A record mapping common event IDs to their respective call counts.
+ * @param targetEventIds A unique list of common event IDs to monitor.
+ *                       Duplicate entries are not allowed and should be filtered beforehand.
+ *                       Although the function will still work with duplicates, it may lead to double counting.
+ * @returns The total number of times the specified common events have been called.
+ */
+export const countCommonEventCall = (
+  eventCallCount: CommonEventCallCount,
+  targetEventIds: number[]
+) => targetEventIds.reduce((acc, id) => acc + eventCallCount[id], 0);
